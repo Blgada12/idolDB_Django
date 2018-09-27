07d88f99f16c0805a9e9e4536402786d8b09942b
blgada12 <blgada12@naver.com>
2018-09-27 21:26:52 +0900

    Beta v09.27.1

--- idolmaster/views.py
+++ idolmaster/views.py
@@ -1,13 +1,51 @@
-from django.shortcuts import render
-from .models import Idol
+from django.shortcuts import render, redirect
+from .models import Idol, Production
 from .utils import get_idol_byId
 from django.utils import timezone
 
-AFTER_INDEX = 2
+AFTER_INDEX = 3
 
 
 def idol_main(req):
-    idols = Idol.objects.all()
+    productions: str = req.GET.get('productions')
+    if productions:
+        req.session['productions'] = productions.split(',')
+        return redirect(idol_main)
+
+    clicked: str = req.GET.get('clicked')
+    if clicked:
+        pro_selected: list = req.session.get('productions')
+        pro_selected.append(clicked)
+        pro_selected.sort()
+        req.session['productions'] = pro_selected
+
+        return redirect(idol_main)
+
+    unclicked: str = req.GET.get('unclicked')
+    if unclicked:
+        pro_selected: list = req.session.get('productions')
+        pro_selected.remove(unclicked)
+        req.session['productions'] = pro_selected
+
+        return redirect(idol_main)
+
+    pro_Objects = Production.objects.all()
+    selected_pro = list()
+    if req.session.get('productions', False):
+        for i in req.session.get('productions'):
+            selected_pro.append(Production.objects.get(id=int(i)))
+    else:
+        tmp = list()
+        for i in pro_Objects:
+            tmp.append(str(i.id))
+            selected_pro.append(i)
+        req.session['productions'] = tmp
+
+    idols = Idol.objects.all().filter(production=selected_pro[0])
+
+    for i in range(1, len(selected_pro)):
+        idols = idols | Idol.objects.all().filter(production=selected_pro[i])
+
     today_time = timezone.now()
     today_idols = idols.filter(birth=today_time)
 
@@ -32,16 +70,63 @@ def idol_main(req):
 
     after_idols_result = list()
 
+
     for i in after_date:
         after_idols_result.append(idols.filter(birth__day=i[1], birth__month=i[0]))
-
     return render(req, 'idoldb/index.html', {'today': today_idols, 'isToday': True if
-    len(today_idols) > 0 else False, 'after': after_idols_result})
+    len(today_idols) > 0 else False, 'after': after_idols_result, 'productions': pro_Objects,
+                                             'selected_pro': selected_pro})
 
 
 def idol_all(req):
+    productions: str = req.GET.get('productions')
+    if productions:
+        req.session['productions'] = productions.split(',')
+        return redirect(idol_all)
+
+    clicked: str = req.GET.get('clicked')
+    if clicked:
+        pro_selected: list = req.session.get('productions')
+        pro_selected.append(clicked)
+        pro_selected.sort()
+        req.session['productions'] = pro_selected
+
+        return redirect(idol_all)
+
+    unclicked: str = req.GET.get('unclicked')
+    if unclicked:
+        pro_selected: list = req.session.get('productions')
+        pro_selected.remove(unclicked)
+        req.session['productions'] = pro_selected
+
+        return redirect(idol_all)
+
     idols = Idol.objects.all()
-    return render(req, 'idoldb/idol_all.html', {'idols': idols})
+
+    productions: str = req.GET.get('production')
+    if productions:
+        req.session['productions'] = productions
+        return redirect(idol_all)
+
+    pro_Objects = Production.objects.all()
+    result = list()
+
+    selected_pro = list()
+    if req.session.get('productions', False):
+        for i in req.session.get('productions'):
+            selected_pro.append(Production.objects.get(id=int(i)))
+    else:
+        tmp = list()
+        for i in pro_Objects:
+            tmp.append(str(i.id))
+            selected_pro.append(i)
+        req.session['productions'] = tmp
+
+    for i in selected_pro:
+        result.extend(idols.filter(production=i))
+
+    return render(req, 'idoldb/idol_all.html',
+                  {'idols': result, 'productions': pro_Objects, 'selected_pro': selected_pro})
 
 
 def idol_detail(req, idol_id):
